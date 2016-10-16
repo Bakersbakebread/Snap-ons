8680f8f288330d0b3c28346574a5be1c18dac3df
SnappyDragon18 <pg61151@gmail.com>
2016-10-16 13:07:10 +0530

    Saitama
    Defeat anyone with the power of One Punch Man!

---
+++ saitama.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Saitama:
+    """Defeat anyone with the power of One Punch Man!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def onepunch(self, user : discord.Member):
+        """Punch!"""
+
+        await self.bot.say("ONE PUNCH! And " + user.mention + " has been KO'd!")
+    @commands.command()
+    async def oneslap(self, user : discord.Member):
+        """Slap!"""
+
+        await self.bot.say("ONE SLAP And " + user.mention + " has been KO'd!")
+    @commands.command()
+    async def onekick(self, user : discord.Member):
+        """Kick!"""
+
+        await self.bot.say("ONE KICK! And " + user.mention + " has been KO'd")
+
+def setup(bot):
+    bot.add_cog(saitama(bot))
