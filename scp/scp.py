f9ab6217b7dd856250d56a2f33730ab98d9febe9
SnappyDragon18 <pg61151@gmail.com>
2016-10-22 18:07:28 +0530

    Update scp.py

--- scp/scp.py
+++ scp/scp.py
@@ -9,7 +9,7 @@ class SCP:
 
     @commands.command()
     async def scp(self, num : int):
-        """Look up SPC articles. Warning: Some of them may be too creepy or gruesome. Reminder: You must specify a number between 1 and 2999"""
+        """Look up SPC articles. Warning: Some of them may be too creepy or gruesome. Reminder: You must specify a number between 1 and 2999."""
 
         #Thanks Shigbeard and Redjumpman for helping me!
 
@@ -17,6 +17,14 @@ class SCP:
              msg = "http://www.scp-wiki.net/scp-{0:03}".format(num)
         else:
              msg = "You must specify a number between 1 and 2999"
+
+        await self.bot.say(msg)
+
+    @commands.command()
+    async def scpj(self, joke : str):
+        """Look up SPC-Js. Reminder: Enter the correct name or else the resultant page will be invalid. (Use 001, etc. in case of numbers less than 100.)"""
+
+        msg = "http://www.scp-wiki.net/scp-{0}-j".format(joke)
 
         await self.bot.say(msg)
 def setup(bot):
