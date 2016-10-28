8b8b5534d3bae316c1fa9a697daae87fe82281a9
SnappyDragon18 <pg61151@gmail.com>
2016-10-28 23:33:56 +0530

    Update say.py

--- say/say.py
+++ say/say.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 
 class Say:
-    """Make Red say stuff!"""
+    """Make Red say stuff! Remember, enclose the sentence you want Red to say in double quotes."""
 
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
     async def say(self, msg : str):
-        """Make Red say stuff!"""
+        """Make Red say stuff! Remember, enclose the sentence you want Red to say in double quotes."""
 
         await self.bot.say(("{0}").format(msg))
 def setup(bot):
