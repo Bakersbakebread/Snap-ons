b3de5364f89d351303957823635fb6a24fb32cf7
SnappyDragon18 <pg61151@gmail.com>
2016-10-29 11:03:03 +0530

    Update say.py

--- say/say.py
+++ say/say.py
@@ -8,9 +8,9 @@ class Say:
         self.bot = bot
         
     @commands.command()
-    async def say(self, msg : str):
+    async def say(self, msg):
         """Make Red say stuff! Remember, enclose the sentence you want Red to say in double quotes."""
 
-        await self.bot.say(("{0}").format(msg))
+        await self.bot.say(msg)
 def setup(bot):
     bot.add_cog(Say(bot))
