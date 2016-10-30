744e4103572e53c6155fa7bf61b0200f9f30b0e1
SnappyDragon18 <pg61151@gmail.com>
2016-10-30 16:01:38 +0530

    Update wizzardcurrency.py

--- wizardcurrency/wizzardcurrency.py
+++ wizardcurrency/wizzardcurrency.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 
 class WizardCurrency:
-    """Convert Real Life currencies into Galleons and Vice Versa! ʛ"""
+    """ʛ: Convert Real Life currencies into Galleons and Vice Versa! To get started, use ``[p]wizconvert``"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def wizconvert(self, src : str, fnl : str, amt : float):
-        """ʛ: Convert Real Life currencies into Galleons and Vice Versa! Supported Currencies: ``galleon, pound, dollar, yen, rupee, euro``"""
+        """ʛ: Convert Real Life currencies into Galleons and Vice Versa! Supported Currencies: ``galleon, pound (uk), dollar (us), yen, rupee (in), euro (eu), ruble (ru), real (br), peso (ph)``"""
         if (src == 'galleon' and fnl == 'pound'):
             ans = amt*4.93
         elif (src == 'pound' and fnl == 'galleon'):
@@ -30,8 +30,20 @@ class WizardCurrency:
             ans = amt*5.90
         elif (src == 'euro' and fnl == 'galleon'):
             ans = amt/5.90
+        elif (src == 'galleon' and fnl == 'ruble'):
+            ans = amt*436,27
+        elif (src == 'ruble' and fnl == 'galleon'):
+            ans = amt/436,27
+        elif (src == 'galleon' and fnl == 'real'):
+            ans = amt*19.87
+        elif (src == 'real' and fnl == 'galleon'):
+            ans = amt/19.87
+        elif (src == 'galleon' and fnl == 'peso'):
+            ans = amt*342.24
+        elif (src == 'peso' and fnl == 'galleon'):
+            ans = amt/342.24
         else:
-            ans = "Source currency and Final currency can only be one of the following: ``galleon, pound, dollar, yen, rupee, euro``"
+            ans = "Source currency and Final currency can only be one of the following: ``galleon, pound (uk), dollar (us), yen, rupee (in), euro (eu), ruble (ru), real (br), peso (ph)``"
         await self.bot.say(ans)
 
 def setup(bot):
