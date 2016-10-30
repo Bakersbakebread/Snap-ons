00dc6c3c0093dccaf606d76ba00b0c94b1829a9c
SnappyDragon18 <pg61151@gmail.com>
2016-10-30 20:35:24 +0530

    Update saitama.py

--- saitama/saitama.py
+++ saitama/saitama.py
@@ -15,17 +15,17 @@ class Saitama:
     async def onepunch(self, user : discord.Member):
         """Punch!"""
 
-        await self.bot.say("ONE PUNCH! And " + user.mention + " has been KO'd!")
+        await self.bot.say("ONE PUNCH!" + user.mention + " has been KO'd with power of One-Punch Man!")
     @commands.command()
     async def oneslap(self, user : discord.Member):
         """Slap!"""
 
-        await self.bot.say("ONE SLAP And " + user.mention + " has been KO'd!")
+        await self.bot.say("ONE SLAP!" + user.mention + " has been KO'd with power of One-Punch Man!")
     @commands.command()
     async def onekick(self, user : discord.Member):
         """Kick!"""
 
-        await self.bot.say("ONE KICK! And " + user.mention + " has been KO'd")
+        await self.bot.say("ONE KICK!" + user.mention + " has been KO'd with power of One-Punch Man!")
 
 def setup(bot):
     bot.add_cog(Saitama(bot))
