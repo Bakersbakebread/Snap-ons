c35798df592e47851a6edfd84a978f2b63bc0074
SnappyDragon18 <pg61151@gmail.com>
2016-10-31 12:35:25 +0530

    Update saitama.py

--- saitama/saitama.py
+++ saitama/saitama.py
@@ -7,21 +7,29 @@ class Saitama:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def onepunch(self, user : discord.Member):
+    @commands.command(pass_context=True)
+    async def onepunch(self, context, user : discord.Member):
         """Punch!"""
 
-        await self.bot.say("ONE PUNCH!" + user.mention + " has been KO'd with power of One-Punch Man!")
-    @commands.command()
-    async def oneslap(self, user : discord.Member):
+        if user.id == self.bot.user.id:
+            await self.bot.say(context.message.author.mention + "**, how dare you try to punch me? **" + context.message.author.mention + "** has been KO\'d with the power of One Punch Man!**")
+        else:
+            await self.bot.say("ONE PUNCH! " + user.mention + " has been KO'd with power of One-Punch Man!")
+    @commands.command(pass_context=True)
+    async def oneslap(self, context, user : discord.Member):
         """Slap!"""
 
-        await self.bot.say("ONE SLAP!" + user.mention + " has been KO'd with power of One-Punch Man!")
-    @commands.command()
-    async def onekick(self, user : discord.Member):
+        if user.id == self.bot.user.id:
+            await self.bot.say(context.message.author.mention + "**, how dare you try to slap me? **" + context.message.author.mention + "** has been KO\'d with the power of One Punch Man!**")
+        else:
+            await self.bot.say("ONE SLAP! " + user.mention + " has been KO'd with power of One-Punch Man!")
+    @commands.command(pass_context=True)
+    async def onekick(self, context, user : discord.Member):
         """Kick!"""
 
-        await self.bot.say("ONE KICK!" + user.mention + " has been KO'd with power of One-Punch Man!")
-
+        if user.id == self.bot.user.id:
+            await self.bot.say(context.message.author.mention + "**, how dare you try to kick me? **" + context.message.author.mention + "** has been KO\'d with the power of One Punch Man!**")
+        else:
+            await self.bot.say("ONE KICK! " + user.mention + " has been KO'd with power of One-Punch Man!")
 def setup(bot):
     bot.add_cog(Saitama(bot))
