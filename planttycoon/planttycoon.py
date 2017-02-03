1c4bd18fe8a4cd36ad7be75e24a5f81ff553ac80
SnappyDragon18 <pg61151@gmail.com>
2017-02-03 16:05:52 +0530

    Update planttycoon.py

--- planttycoon/planttycoon.py
+++ planttycoon/planttycoon.py
@@ -216,22 +216,22 @@ class PlantTycoon:
             #
 
             if month == 1:
-                self.plants['plants'].append({"name": "Tanabata Tree", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/FD38JJj.jpg", "health": 100, "degradation": 4.25, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Tanabata Tree", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/FD38JJj.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
             elif month == 2:
-                self.plants['plants'].append({"name": "Chocolate Rose", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/Sqg6pcG.jpg", "health": 100, "degradation": 5.5, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Chocolate Rose", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/Sqg6pcG.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
             elif month == 3:
-                self.plants['plants'].append({"name": "Shamrock", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/kVig04M.jpg", "health": 100, "degradation": 5.5, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Shamrock", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/kVig04M.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
             elif month == 4:
-                self.plants['plants'].append({"name": "Easter Egg Eggplant", "article": "an", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/5jltGQa.jpg", "health": 5.5, "degradation": 0, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Easter Egg Eggplant", "article": "an", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/5jltGQa.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
             elif month == 10:
-                self.plants['plants'].append({"name": "Jack O' Lantern", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/efApsxG.jpg", "health": 5.5, "degradation": 0, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Jack O' Lantern", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/efApsxG.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
             elif month == 12:
-                self.plants['plants'].append({"name": "Holly", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/maDLmJC.jpg", "health": 100, "degradation": 5.5, "threshold": 110, "badge": "Annualsary", "reward": 21600})
+                self.plants['plants'].append({"name": "Holly", "article": "a", "time": 70800, "rarity": "event", "image": "http://i.imgur.com/maDLmJC.jpg", "health": 100, "degradation": 9, "threshold": 110, "badge": "Annualsary", "reward": 21600})
                 event_plant = True
 
             #
