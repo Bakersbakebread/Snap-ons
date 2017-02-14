56074ec37e9665fe8e61a41c7545b7330d927da8
SnappyDragon18 <pg61151@gmail.com>
2017-02-14 22:47:34 +0530

    Embed coloured.

--- unicode/unicode.py
+++ unicode/unicode.py
@@ -15,30 +15,37 @@ class Unicode:
         """Encode/Decode a Unicode character."""
         if context.invoked_subcommand is None:
             await self.bot.send_cmd_help(context)
-    @unicode.command()
-    async def encode(self, character):
-        """Encode a Unicode character."""
-        try:
-            data = 'U+{:04X}'.format(ord(character[0]))
-        except ValueError:
-            data = '<unknown>'
-        em = discord.Embed(title=character ,description=data)
-        await self.bot.say(embed=em)
-        
+            
     @unicode.command()
     async def decode(self, character):
         """Decode a Unicode character."""
         try:
+            data = 'U+{:04X}'.format(ord(character[0]))
+            color=discord.Color.green()
+        except ValueError:
+            data = '<unknown>'
+            color=discord.Color.red()
+        em = discord.Embed(title=character, description=data, color=color)
+        await self.bot.say(embed=em)
+        
+    @unicode.command()
+    async def encode(self, character):
+        """Encode an Unicode character."""
+        try:
             if character[:2] == '\\u':
                 data = repr(c.decode(character, 'unicode-escape'))
                 data = data.strip("'")
+                color=discord.Color.green()
             elif character[:2] == 'U+':
                 data = chr(int(character.lstrip('U+'), 16))
+                color=discord.Color.green()
             else:
                 data = '<unknown>'
+                color=discord.Color.red()
         except ValueError:
             data = '<unknown>'
-        em = discord.Embed(title=character ,description=data)
+            color=discord.Color.red()
+        em = discord.Embed(title=character, description=data, color=color)
         await self.bot.say(embed=em)
 def setup(bot):
     bot.add_cog(Unicode(bot))
